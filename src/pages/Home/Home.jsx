65e6340f95c0b51609533e5473237f5c740321c2
Roman lesyo <ladiginscormag@gmail.com>
2023-04-06 16:07:53 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
-import imgMob from '../../img/HomePageDog/img@1x.png';
+// import imgMob from '../../img/HomePageDog/img@1x.png';
 import imgTab from '../../img/HomePageDog/img@1x.png';
 import imgDes from '../../img/HomePageDog/img@1x.png';
 // import WaveMob from '../../img/HomePageBG/Wave@1x.png';
-// import WaveTab from '../../img/HomePageBG/Wave@2x.png';
+import WaveTab from '../../img/HomePageBG/Wave@2x.png';
 import WaveDes from '../../img/HomePageBG/Wave@3x.png';
-// import bgMob from '../../img/HomePageDog/bg@1x.png';
-// import bgTab from '../../img/HomePageDog/bg@2x.png';
+import bgMob from '../../img/HomePageDog/bg@1x.png';
+import bgTab from '../../img/HomePageDog/bg@2x.png';
 import bgDes from '../../img/HomePageDog/bg@3x.png';
 
 export const HomePageBG = styled.div`
@@ -19,20 +19,20 @@ export const HomePageBG = styled.div`
   min-height: 600px;
 
   background-repeat: no-repeat;
-  background-position: bottom;
-  background-size: 280px 364px, 620px 470px;
-
-  background-image: url(${imgMob});
+  background-position: -330px 20px;
+  background-size: 650px 470px;
+  background-image: url(${bgMob});
 
   @media screen and (min-width: 768px) {
-    background-size: 700px 850px, 1449px 1094px;
-    min-height: 1250px;
-    background-image: url(${imgTab});
+    background-size: 300px 300px, 700px 600px, 800px 900px;
+    background-position: 45px 265px, -100px 20px, -30px 0px;
+    min-height: 400px;
+    background-image: url(${imgTab}), url(${WaveTab}), url(${bgTab});
   }
 
   @media screen and (min-width: 1280px) {
     background-size: 424px 455px, 700px 538px, 500px 500px;
-    min-height: 300px;
+    min-height: 320px;
     background-position: 585px 220px, 600px 110px, 20px 130px;
     background-image: url(${imgDes}), url(${bgDes}), url(${WaveDes});
   }
@@ -83,9 +83,7 @@ const Home = () => {
     <>
       <HomePageBG>
         <HomePageContainer>
-          <HomeTitle>
-            Хороший майстер
-          </HomeTitle>
+          <HomeTitle>Хороший майстер</HomeTitle>
         </HomePageContainer>
       </HomePageBG>
     </>
