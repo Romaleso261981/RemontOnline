4e776d2a8e6766d44550a4111aafb99ac60b3136
Roman lesyo <ladiginscormag@gmail.com>
2023-04-06 11:13:32 +0300

    set up a deployment on netflix

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Global styles={GlobalStyles} />
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/RemontOnline">
+          <BrowserRouter>
             <App />
           </BrowserRouter>
         </PersistGate>
