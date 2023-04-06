27ef418f689ed0c54c9789f54dea6844b6ac3697
Roman lesyo <ladiginscormag@gmail.com>
2023-04-06 11:40:22 +0300

    Update API.js

--- src/API.js
+++ src/API.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const API = axios.create({
   // baseURL: 'http://remontonlineback-production.up.railway.app/',
-  baseURL: 'https://petly-site-back.up.railway.app/',
+  // baseURL: 'https://petly-site-back.up.railway.app/',
   // baseURL: 'http://localhost:8000',
-  // baseURL: 'http://185.233.118.244:8000',
+  baseURL: 'http://185.233.118.244:8000',
 });
 
 const authToken = {
