55f4f00a0057f3e0f240629534fb6d3c8eccd7fb
Roman lesyo <ladiginscormag@gmail.com>
2023-04-25 10:33:20 +0300

    some bag fix

--- src/components/Header/components/Navigation/Navigation.jsx
+++ src/components/Header/components/Navigation/Navigation.jsx
@@ -1,14 +1,15 @@
-import { MenuButtonOpen } from '../MobileMenu/MenuButton/MenuButtonOpen.styled';
-import { Nav, AuthNav, UserNav } from './components';
+// import { MenuButtonOpen } from '../MobileMenu/MenuButton/MenuButtonOpen.styled';
+import { Nav, AuthNav } from './components';
+// import { Nav, AuthNav, UserNav } from './components';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
+// import { useSelector } from 'react-redux';
+// import { selectIsLoggedIn } from 'redux/auth/auth-selectors';
 import { NavMobile } from '../MobileMenu/MobileMenu';
 import { NavContainer } from './Navigation.styled';
 
 export const Navigation = () => {
   const [showMenuModal, setShowMenuModal] = useState(false);
-  const isloggedIn = useSelector(selectIsLoggedIn);
+  // const isloggedIn = useSelector(selectIsLoggedIn);
 
   const onMenuButtonClick = () => {
     setShowMenuModal(!showMenuModal);
@@ -17,8 +18,9 @@ export const Navigation = () => {
   return (
     <NavContainer>
       <Nav />
-      {isloggedIn ? <UserNav /> : <AuthNav />}
-      <MenuButtonOpen onMenuButtonClick={onMenuButtonClick} />
+      {/* {isloggedIn ? <UserNav /> : <AuthNav />} */}
+      <AuthNav />
+      {/* <MenuButtonOpen onMenuButtonClick={onMenuButtonClick} /> */}
       {showMenuModal && <NavMobile onClose={onMenuButtonClick} />}
     </NavContainer>
   );
