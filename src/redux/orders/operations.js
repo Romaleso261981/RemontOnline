690f6d4d986a8b74de17b8d451d93aca139c8e60
lesoRoman <ladiginscormag@gmail.com>
2023-08-01 10:04:35 +0300

    some bag fix

--- src/redux/orders/operations.js
+++ src/redux/orders/operations.js
@@ -41,9 +41,11 @@ export const addOrder = createAsyncThunk(
     }
   },
 );
+
 export const done = createAsyncThunk('order/done', async (orderId, thunkAPI) => {
   try {
-    const response = await API.post(`/orders/${orderId}`, { orderId });
+    const response = await API.post(`/orders/done/${orderId}`, { orderId });
+    console.log(response.data);
     return response.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
